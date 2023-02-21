d8580729f1da65f2b9a599f5442dd517fb0ac3cb
Alex Zhidkov <sashazudkov@gmail.com>
2023-02-21 19:51:25 +0200

    added try/catch/await

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import FingerprintJS from '@fingerprintjs/fingerprintjs-pro'
 import { apiKeyUrl, eventHandlerUrl } from './config/config'
-
 async function getApiKey (url, projectApiKey) {
   const response = await fetch(url, {
         method: 'GET',
@@ -19,34 +18,41 @@ async function getApiKey (url, projectApiKey) {
 async function recure(userId, projectApiKey, eventType) {
   const recureApiKey = await getApiKey(apiKeyUrl, projectApiKey)
   const fpPromise = FingerprintJS.load({ apiKey: recureApiKey })
-  fpPromise
-  .then(fp => fp.get())
-  .then(result => {
-      const timestamp = new Date().toISOString();
-      const xApiKey = projectApiKey;
-      
-      const data = {
-        'userId': userId.toString(),
-        'provider': 'fingerprint',
-        'eventType': eventType,
-        'visitorId': result.visitorId,
-        'visitorFound': result.visitorFound,
-        'confidence': result.confidence,
-        'timestamp': timestamp
 
-      };
+  try {
+    await fpPromise
+      .then(fp => fp.get())
+      .then(result => {
+        const timestamp = new Date().toISOString();
+        const xApiKey = projectApiKey;
+        
+        const data = {
+          'userId': userId.toString(),
+          'provider': 'fingerprint',
+          'eventType': eventType,
+          'visitorId': result.visitorId,
+          'visitorFound': result.visitorFound,
+          'confidence': result.confidence,
+          'timestamp': timestamp
 
-      await fetch(eventHandlerUrl, {
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'x-api-key': xApiKey
-      },
-      body: JSON.stringify(data)
+        };
+
+        await fetch(eventHandlerUrl, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'x-api-key': xApiKey
+        },
+        body: JSON.stringify(data)
+        })
       })
-  })
-  .catch(error => console.error(error))
+
+  } catch (error) {
+
+    console.error(error);
+  }
+
 }
 
 module.exports = recure;
