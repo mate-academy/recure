bbd55042cf75e881da2adea133b13d829c39ffa1
Alex Zhidkov <sashazudkov@gmail.com>
2023-02-22 13:23:39 +0200

    [Recure]: added enum, fixed code style

--- index.ts
+++ index.ts
@@ -1,63 +1,68 @@
+'use strict'
 import FingerprintJS from '@fingerprintjs/fingerprintjs-pro'
 import { apiKeyUrl, eventHandlerUrl } from './config/config'
+import { EventType } from './event/eventType';
 
-type Data = {
+type Payload = {
   userId: string,
   provider: string,
-  type: string,
+  type: EventType,
   visitorId: string,
   visitorFound: boolean,
   confidence: object,
   timestamp: string
 }
 
+type Response = {
+    apiKey: string,
+}
+
 async function getApiKey (url: string, projectApiKey: string): Promise<string> {
   const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'x-api-key': projectApiKey
-      },
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'x-api-key': projectApiKey
+    },
   });
 
-  const data: any = await response.json();
+  const data: Response = await response.json();
 
   return data.apiKey;
 }
 
-async function recure(userId: number | string, projectApiKey: string, eventType: string): Promise<any> {
-  const recureApiKey: string = await getApiKey(apiKeyUrl, projectApiKey)
-  const fpPromise: Promise<any> = FingerprintJS.load({ apiKey: recureApiKey })
+async function recure(userId: number | string, projectApiKey: string, eventType: EventType): Promise<any> {
+  const recureApiKey: string = await getApiKey(apiKeyUrl, projectApiKey);
+  const fp: any = await FingerprintJS.load({ apiKey: recureApiKey })
 
   try {
-    await fpPromise
-      .then((fp: any) => fp.get())
-      .then(async (result: any) => {
-        const timestamp: string = new Date().toISOString();
-        const xApiKey: string = projectApiKey;
-        
-        const data: Data = {
-          userId: userId.toString(),
-          provider: 'fingerprint',
-          type: eventType,
-          visitorId: result.visitorId,
-          visitorFound: result.visitorFound,
-          confidence: result.confidence,
-          timestamp: timestamp
 
-        };
+    const result: any = await fp.get();
 
-        await fetch(eventHandlerUrl, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'x-api-key': xApiKey
-        },
-        body: JSON.stringify(data)
-        })
-      })
+    const timestamp: string = new Date().toISOString();
+    const xApiKey: string = projectApiKey;
+
+    const payload: Payload = {
+      userId: userId.toString(),
+      provider: 'fingerprint',
+      type: eventType,
+      visitorId: result.visitorId,
+      visitorFound: result.visitorFound,
+      confidence: result.confidence,
+      timestamp: timestamp
+
+    };
+
+    await fetch(eventHandlerUrl, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'x-api-key': xApiKey
+      },
+    body: JSON.stringify(payload),
+    })
 
   } catch (error) {
 
@@ -67,3 +72,4 @@ async function recure(userId: number | string, projectApiKey: string, eventType:
 }
 
 module.exports = recure;
+module.exports = EventType;
