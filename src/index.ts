7a653bbad1c82fc33eac19de2ee7040586c1de78
Alex Zhidkov <sashazudkov@gmail.com>
2023-02-28 16:44:12 +0200

    [Recure]: added cookies saver for visitorId/refactor some unnecessary imports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 "use strict";
 import FingerprintJS from "@fingerprintjs/fingerprintjs-pro";
 
-export const apiKeyUrl: string = 'https://api.recure.ai/api/event_handler/get_api_key/';
-export const eventHandlerUrl: string = 'https://api.recure.ai/api/event_handler/';
+const apiKeyUrl: string = 'https://api.recure.ai/api/event_handler/get_api_key/';
+const eventHandlerUrl: string = 'https://api.recure.ai/api/event_handler/';
+const expiresDate: number = 60;
 
 export enum EventType {
   LOGIN = 'LOGIN',
@@ -14,22 +15,20 @@ export enum EventType {
   SUBSCRIPTION_ENDED = 'SUBSCRIPTION_ENDED'
 }
 
-export type Payload = {
+type Payload = {
   userId: string;
   provider: string;
   type: EventType;
   visitorId: string;
-  visitorFound: boolean;
-  confidence: object;
   timestamp: string;
   eventName?: string | undefined;
 };
 
-export type KeyResponse = {
+type KeyResponse = {
   apiKey: string;
 };
 
-export async function getApiKey(
+async function getApiKey(
   url: string,
   projectApiKey: string
 ): Promise<string> {
@@ -47,6 +46,67 @@ export async function getApiKey(
   return data.apiKey;
 }
 
+function setCookie(cookieName: string, cookieValue: string, exdays: number) {
+  const date = new Date();
+  date.setTime(date.getTime() + (exdays*24*60*60*1000));
+  const expires: string = "expires="+ date.toUTCString();
+  document.cookie = `${cookieName}=${cookieValue};${expires};path=/`;
+}
+
+function getCookie(cookieName: string): string {
+  const name: string = cookieName + "=";
+  const decodedCookie: string = decodeURIComponent(document.cookie);
+
+  const cookiesParts: string[] = decodedCookie.split(`; ${name}`);
+
+  if (cookiesParts.length === 1) {
+    return "";
+  }
+
+  const visitorId: string | undefined = cookiesParts.pop()?.split(";")[0];
+
+  return visitorId || "";
+}
+
+async function getOrSetVisitorId(cookieName: string, recureApiKey: string): Promise<string> {
+  const visitorId: string = getCookie(cookieName);
+
+  if (visitorId !== "") {
+    return visitorId;
+  }
+
+  const result: any = await getFingerPrintResult(recureApiKey);
+
+  setCookie(cookieName, result.visitorId, expiresDate);
+
+  return result.visitorId;
+}
+
+async function getFingerPrintResult(recureApiKey: string): Promise<any> {
+    const fp: any = await FingerprintJS.load({ apiKey: recureApiKey });
+
+    return await fp.get()
+}
+
+async function getPayload(
+  recureApiKey: string,
+  userId: number | string,
+  eventType: EventType,
+  eventName?: string | undefined
+): Promise<Payload> {
+
+  const visitorId = await getOrSetVisitorId("visitorId", recureApiKey)
+
+  return {
+    userId: userId.toString(),
+    provider: "fingerprint",
+    type: eventType,
+    visitorId,
+    timestamp: new Date().toISOString(),
+    eventName: eventName || "",
+  };
+}
+
 export async function recure(
   userId: number | string,
   projectApiKey: string,
@@ -54,29 +114,16 @@ export async function recure(
   eventName?: string | undefined
 ): Promise<any> {
   const recureApiKey: string = await getApiKey(apiKeyUrl, projectApiKey);
-  const fp: any = await FingerprintJS.load({ apiKey: recureApiKey });
 
   try {
-    const result: any = await fp.get();
-    const xApiKey: string = projectApiKey;
-
-    const payload: Payload = {
-      userId: userId.toString(),
-      provider: "fingerprint",
-      type: eventType,
-      visitorId: result.visitorId,
-      visitorFound: result.visitorFound,
-      confidence: result.confidence,
-      timestamp: new Date().toISOString(),
-      eventName: eventName || "",
-    };
+    const payload: Payload = await getPayload(recureApiKey, userId, eventType, eventName)
 
     await fetch(eventHandlerUrl, {
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
-        "x-api-key": xApiKey,
+        "x-api-key": projectApiKey,
       },
       body: JSON.stringify(payload),
     });
