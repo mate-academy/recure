dc2c06c21de92c2d461289683bb65d32c4b9e4a1
Alex Zhidkov <72562232+Abnormaltype@users.noreply.github.com>
2023-03-01 11:45:46 +0200

    [Recure]: added cookies saver for visitorId/refactor some unnecessary… (#4)
    * [Recure]: added cookies saver for visitorId/refactor some unnecessary imports
    
    * [Recure]: fixed typos
    
    * [Recure]: added js-cookies instead of document.cookies
    
    * 2.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 "use strict";
 import FingerprintJS from "@fingerprintjs/fingerprintjs-pro";
+import * as Cookies from 'js-cookie';
 
-export const apiKeyUrl: string = 'https://api.recure.ai/api/event_handler/get_api_key/';
-export const eventHandlerUrl: string = 'https://api.recure.ai/api/event_handler/';
+const apiKeyUrl: string = 'https://api.recure.ai/api/event_handler/get_api_key/';
+const eventHandlerUrl: string = 'https://api.recure.ai/api/event_handler/';
+const daysToExpire: number = 60;
 
 export enum EventType {
   LOGIN = 'LOGIN',
@@ -14,22 +16,20 @@ export enum EventType {
   SUBSCRIPTION_ENDED = 'SUBSCRIPTION_ENDED'
 }
 
-export type Payload = {
+type Payload = {
   userId: string;
   provider: string;
   type: EventType;
   visitorId: string;
-  visitorFound: boolean;
-  confidence: object;
   timestamp: string;
   eventName?: string | undefined;
 };
 
-export type KeyResponse = {
+type KeyResponse = {
   apiKey: string;
 };
 
-export async function getApiKey(
+async function getApiKey(
   url: string,
   projectApiKey: string
 ): Promise<string> {
@@ -47,6 +47,45 @@ export async function getApiKey(
   return data.apiKey;
 }
 
+async function getOrSetVisitorId(cookieName: string, recureApiKey: string): Promise<string> {
+  const visitorId: string | undefined = Cookies.get(cookieName);
+
+  if (visitorId !== "" && visitorId !== undefined) {
+    return visitorId;
+  }
+
+  const result: any = await getFingerPrintResult(recureApiKey);
+
+  Cookies.set(cookieName, result.visitorId, { expires: daysToExpire, path: '' });
+
+  return result.visitorId;
+}
+
+async function getFingerPrintResult(recureApiKey: string): Promise<any> {
+    const fp: any = await FingerprintJS.load({ apiKey: recureApiKey });
+
+    return await fp.get();
+}
+
+async function getPayload(
+  recureApiKey: string,
+  userId: number | string,
+  eventType: EventType,
+  eventName?: string | undefined
+): Promise<Payload> {
+
+  const visitorId = await getOrSetVisitorId("visitorId", recureApiKey)
+
+  return {
+    userId: userId.toString(),
+    provider: "fingerprint",
+    type: eventType,
+    visitorId,
+    timestamp: new Date().toISOString(),
+    eventName: eventName || "",
+  };
+}
+
 export async function recure(
   userId: number | string,
   projectApiKey: string,
@@ -54,29 +93,16 @@ export async function recure(
   eventName?: string | undefined
 ): Promise<any> {
   const recureApiKey: string = await getApiKey(apiKeyUrl, projectApiKey);
-  const fp: any = await FingerprintJS.load({ apiKey: recureApiKey });
 
   try {
-    const result: any = await fp.get();
-    const xApiKey: string = projectApiKey;
-
-    const payload: Payload = {
-      userId: userId.toString(),
-      provider: "fingerprint",
-      type: eventType,
-      visitorId: result.visitorId,
-      visitorFound: result.visitorFound,
-      confidence: result.confidence,
-      timestamp: new Date().toISOString(),
-      eventName: eventName || "",
-    };
+    const payload: Payload = await getPayload(recureApiKey, userId, eventType, eventName);
 
     await fetch(eventHandlerUrl, {
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
-        "x-api-key": xApiKey,
+        "x-api-key": projectApiKey,
       },
       body: JSON.stringify(payload),
     });
