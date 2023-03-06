3633419206aeecc00c616637c49754ea02efb70c
Alex Zhidkov <sashazudkov@gmail.com>
2023-03-06 11:26:26 +0200

    [Recure]: added cdn support

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
-"use strict";
-// @ts-ignore
-import FingerprintJS from "@fingerprintjs/fingerprintjs-pro";
-import * as Cookies from "js-cookie";
+'use strict';
+import FingerprintJS from '@fingerprintjs/fingerprintjs-pro';
+import * as Cookies from 'js-cookie';
 
-const apiKeyUrl: string = "https://api.recure.ai/api/event_handler/get_api_key/";
-const eventHandlerUrl: string = "https://api.recure.ai/api/event_handler/";
+const apiKeyUrl: string = 'https://api.recure.ai/api/event_handler/get_api_key/';
+const eventHandlerUrl: string = 'https://api.recure.ai/api/event_handler/';
 const daysToExpire: number = 60;
 const minutesToExpire: number = 5;
-const sendingEventsBlocked = "recureSendingEventsBlocked"
+const sendingEventsBlocked = 'recureSendingEventsBlocked';
 
 export enum EventType {
-  LOGIN = "LOGIN",
-  SIGN_UP = "SIGN_UP",
-  PAGE = "PAGE",
-  FREE_TRIAL_STARTED = "FREE_TRIAL_STARTED",
-  FREE_TRIAL_ENDED = "FREE_TRIAL_ENDED",
-  SUBSCRIPTION_STARTED = "SUBSCRIPTION_STARTED",
-  SUBSCRIPTION_ENDED = "SUBSCRIPTION_ENDED"
+  LOGIN = 'LOGIN',
+  SIGN_UP = 'SIGN_UP',
+  PAGE = 'PAGE',
+  FREE_TRIAL_STARTED = 'FREE_TRIAL_STARTED',
+  FREE_TRIAL_ENDED = 'FREE_TRIAL_ENDED',
+  SUBSCRIPTION_STARTED = 'SUBSCRIPTION_STARTED',
+  SUBSCRIPTION_ENDED = 'SUBSCRIPTION_ENDED',
 }
 
-export type EventOptions = {
-  pageName: string;
-} | undefined;
+export type EventOptions =
+  | {
+      pageName: string;
+    }
+  | undefined;
 
 type Payload = {
   userId: string;
@@ -36,16 +37,13 @@ type KeyResponse = {
   apiKey: string;
 };
 
-async function getApiKey(
-  url: string,
-  projectApiKey: string
-): Promise<string> {
+async function getApiKey(url: string, projectApiKey: string): Promise<string> {
   const response = await fetch(url, {
-    method: "GET",
+    method: 'GET',
     headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      "x-api-key": projectApiKey,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'x-api-key': projectApiKey,
     },
   });
 
@@ -57,35 +55,34 @@ async function getApiKey(
 async function getOrSetVisitorId(cookieName: string, recureApiKey: string): Promise<string> {
   const visitorId: string | undefined = Cookies.get(cookieName);
 
-  if (visitorId !== "" && visitorId !== undefined) {
+  if (visitorId !== '' && visitorId !== undefined) {
     return visitorId;
   }
 
   const result: any = await getFingerPrintResult(recureApiKey);
 
-  Cookies.set(cookieName, result.visitorId, { expires: daysToExpire, path: "" });
+  Cookies.set(cookieName, result.visitorId, { expires: daysToExpire, path: '' });
 
   return result.visitorId;
 }
 
 async function getFingerPrintResult(recureApiKey: string): Promise<any> {
-    const fp: any = await FingerprintJS.load({ apiKey: recureApiKey });
+  const fp: any = await FingerprintJS.load({ apiKey: recureApiKey });
 
-    return await fp.get();
+  return await fp.get();
 }
 
 async function getPayload(
   recureApiKey: string,
   userId: number | string,
   eventType: EventType,
-  eventOptions?: EventOptions
+  eventOptions?: EventOptions,
 ): Promise<Payload> {
-
-  const visitorId = await getOrSetVisitorId("visitorId", recureApiKey)
+  const visitorId = await getOrSetVisitorId('visitorId', recureApiKey);
 
   return {
     userId: userId.toString(),
-    provider: "fingerprint",
+    provider: 'fingerprint',
     type: eventType,
     visitorId,
     timestamp: new Date().toISOString(),
@@ -98,23 +95,22 @@ function isReadyToSend(): boolean {
 
   if (readyToSend === undefined) {
     const inFiveMinutes: Date = new Date(new Date().getTime() + minutesToExpire * 60 * 1000);
-    Cookies.set(sendingEventsBlocked, "false", { expires: inFiveMinutes, path: "" });
+    Cookies.set(sendingEventsBlocked, 'false', { expires: inFiveMinutes, path: '' });
 
-    return true
+    return true;
   }
 
-  return false
+  return false;
 }
 
 export async function recure(
   userId: number | string,
   projectApiKey: string,
   eventType: EventType,
-  eventOptions?: EventOptions
+  eventOptions?: EventOptions,
 ): Promise<any> {
-
   if (eventType === EventType.PAGE && !isReadyToSend()) {
-      return;
+    return;
   }
 
   const recureApiKey: string = await getApiKey(apiKeyUrl, projectApiKey);
@@ -123,11 +119,11 @@ export async function recure(
     const payload: Payload = await getPayload(recureApiKey, userId, eventType, eventOptions);
 
     await fetch(eventHandlerUrl, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "x-api-key": projectApiKey,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'x-api-key': projectApiKey,
       },
       body: JSON.stringify(payload),
     });
